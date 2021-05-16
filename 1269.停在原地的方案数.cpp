7c87a31a2ddf57b054eefa7d05ba022695cf1b01
EaueauOops <dxy2007sng@126.com>
2021-05-16 11:17:11 +0800

    动态规划 状态转移方程/设置好最初状态值

--- 1269.停在原地的方案数.cpp
+++ 1269.停在原地的方案数.cpp
@@ -8,7 +8,23 @@
 class Solution {
 public:
     int numWays(int steps, int arrLen) {
-
+        int res = 0 ;
+        int maxcolumn = min(arrLen-1,steps);
+        vector<vector<int>> dp(steps+1,vector<int>(maxcolumn+1));
+        int M = 1000000007;
+        dp[0][0] = 1;
+        for(int i =1 ;i<=steps;i++)
+        {
+            for(int j = 0;j<=maxcolumn;j++)
+            {
+                dp[i][j] = dp[i-1][j] % M;
+                if(j-1 >= 0)
+                dp[i][j] = (dp[i][j]+dp[i-1][j-1])%M;
+                if(j+1<=maxcolumn)
+                dp[i][j] = (dp[i][j]+dp[i-1][j+1])%M;
+            }
+        }
+        return dp[steps][0];
     }
 };
 // @lc code=end
