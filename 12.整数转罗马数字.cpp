3e9911442600ed0202251e39e169e3cbe97409f1
EaueauOops <dxy2007sng@126.com>
2021-05-18 19:45:12 +0800

    pair是结构体 不可以在类里初始化

--- 12.整数转罗马数字.cpp
+++ 12.整数转罗马数字.cpp
@@ -5,14 +5,28 @@
  */
 
 // @lc code=start
+    const pair<string,int> r[] = {
+       {"M",1000},{"CM",900},{"D",500},{"CD",400},{"C",100},{"XC",90},{"L",50},{"XL",40},{"X",10},{"IX",9},{"V",5},{"IV",4},{"I",1}
+   };
+
 class Solution {
+
 public:
-    unordered_map <char,int> R = {
-        {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
-    };
+  
     string intToRoman(int num) {
         string res;
-        
+        while(num)
+        {
+           for(int i = 0;i < 13; i++ )
+           {
+               if(num / r[i].second)
+               {
+                   res = res + r[i].first;
+                   num -=  r[i].second;
+                   break;
+               }
+           }  
+        }
         return res;
     }
 };
